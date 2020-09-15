029e22123021ad68a6309992efdbbeff43bd2101
pandora024 <phin-phis@hotmail.com>
2020-09-15 15:35:48 +0700

    ch4

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import WordCard from './WordCard';
+import './App.css';
+    const word = "Hello";
+function App() {
+ return (
+  <div>
+    {
+  <WordCard value="hello"/>
+    }
+  </div>
+
+  );
+}
+
+export default App;
+
